cb08426d2bb63e0a5f4760bf28da020f1fc2bf1e
Kirthik24 <kirthikcode@gmail.com>
2022-08-28 21:09:19 +0530

    The first commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,24 @@ void main() {
   runApp(const MyApp());
 }
 
+class ContactClass{
+  final String name;
+  final String department;
+  final String phone;
+
+
+  const ContactClass({
+    required this.name,
+    required this.department,
+    required this.phone,
+  });
+
+  static ContactClass fromJson(json) => ContactClass(
+      name: json['name'],
+      department: json['department'],
+      phone: json['phone']);
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -18,7 +36,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ContactPage(),
+      home: const MyHomePage(title: 'skct'),
     );
   }
 }
@@ -34,7 +52,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> depList = ["CSE", "IT", "ECE", "MECH"];
+  final List<String> depList = ["cse", "it", "ece", "mech"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,6 +66,14 @@ class _MyHomePageState extends State<MyHomePage> {
               title: Center(
                 child: Text(depList[index].toString()),
               ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ContactPage(destination: depList[index].toString())),
+
+                  );
+              }
             ),
           ),
         ),
@@ -59,64 +85,93 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
 class ContactPage extends StatefulWidget {
-  const ContactPage({Key? key}) : super(key: key);
+  final String destination;
+
+  const ContactPage({Key? key, required this.destination}) : super(key: key);
 
   @override
   State<ContactPage> createState() => _ContactPageState();
 }
 
 class _ContactPageState extends State<ContactPage> {
-  List _contacts = [];
+  late Future<List<ContactClass>> contactsFuture;
 
-  // Fetch content from the json file
-  Future<void> readJson() async {
-    final String response = await rootBundle.loadString('assets/cse.json');
-    final data = await json.decode(response);
-    setState(() {
-      _contacts = data["contacts"];
-    });
+  @override
+  void initState(){
+    super.initState();
+
+    contactsFuture = getContacts(context);
+  }
+
+
+
+   Future<List<ContactClass>> getContacts(BuildContext context) async{
+      final assetBundle = DefaultAssetBundle.of(context);
+      final String address = 'assets/${widget.destination}.json';
+      final data = await assetBundle.loadString(address);
+
+      final body = json.decode(data);
+      return body.map<ContactClass>(ContactClass.fromJson).toList();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'SKCT Contacts',
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Column(
-          children: [
-            ElevatedButton(
-              child: const Text('Load Data'),
-              onPressed: readJson,
-            ),
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text('Contacts List'),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: FutureBuilder<List<ContactClass>>(
+        future:  contactsFuture,
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            final contacts = snapshot.data!;
+            return buildContacts(contacts);
+          }else{
+            return Text('assets/${widget.destination}.json');
+          }
+        },
+      )
+    ),
+  );
 
-            // Display the data loaded from sample.json
-            _contacts.isNotEmpty
-                ? Expanded(
-              child: ListView.builder(
-                itemCount: _contacts.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    margin: const EdgeInsets.all(10),
-                    child: ListTile(
-                      leading: Text(_contacts[index]["name"]),
-                      title: Text(_contacts[index]["department"]),
-                      subtitle: Text(_contacts[index]["phone"]),
-                    ),
-                  );
-                },
-              ),
-            )
-                : Container()
-          ],
-        ),
-      ),
-    );
-  }
+  Widget buildContacts(List<ContactClass> contacts) => ListView.builder(
+      itemCount: contacts.length,
+      itemBuilder: (context, index) {
+        final thiscontact = contacts[index];
+
+        return Card(
+          child: ListTile(
+            title: Text(thiscontact.name),
+            subtitle: Text(thiscontact.department),
+            onTap: (){
+              Navigator.of(context).push(MaterialPageRoute
+                (builder: (context) => ContactDetails(contact: thiscontact),
+              ));
+            },
+          ),
+        );
+
+      },
+  );
 }
+
+class ContactDetails extends StatelessWidget {
+  final ContactClass contact;
+  const ContactDetails({
+    Key? key,
+    required this.contact,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text(contact.name),
+    ),
+    body: Center(
+      child: Text(contact.phone),
+    ),
+  );
+}
+
 
