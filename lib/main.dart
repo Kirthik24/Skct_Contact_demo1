748ce3a176d4565d298827235b20717f9b49b82f
Kirthik24 <kirthikcode@gmail.com>
2022-08-30 22:57:26 +0530

    git check

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'skct'),
+      home: const MyHomePage(title: 'skctr'),
     );
   }
 }
