9ed8795f951193dbded6195a7b0055066955f654
Kirthik24 <kirthikcode@gmail.com>
2022-09-02 13:13:48 +0530

    home page v2 update 1

--- lib/pages/MyHomePageV2.dart
+++ lib/pages/MyHomePageV2.dart
@@ -14,47 +14,76 @@ class _MyHomePageV2State extends State<MyHomePageV2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Skct-Contacts"),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-        Text("Skct Contacts"),
-        Spacer(),
-        Center(
-          child:Container(
-            height: 500,
-
-            child: ListView.builder(
-              itemCount: depList.length,
-              itemBuilder: (context,index) => Card(
-                elevation: 6,
-                margin: const EdgeInsets.all(10),
-                child: ListTile(
-                    shape: RoundedRectangleBorder(
-                        side: BorderSide(color: Colors.blue.shade200, width: 0.5),
-                        borderRadius: BorderRadius.circular(5)),
-                    tileColor: Colors.blue.shade400,
-                    textColor: Colors.white,
-                    iconColor: Colors.white,
-                    title: Center(
-                      child: Text(depList[index].toString()),
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ContactPage(destination: depList[index].toString())),
-
-                      );
-                    }
-                ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(15, 25, 15, 15),
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              child: Row(
+                children: [
+                  Container(
+                    width:130,
+                      child: Image.asset('assets/skctpng.png')),
+                  const Expanded(
+                      child:Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text("Sri Krishna College of Technology",
+                    style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black),
+                  ),
+                      ) )
+                ],
               ),
             ),
-          ) ,
-        )
-        ],
+            const SizedBox(
+              width: 150.0,
+              height: 10.0,
+              child: Divider(
+                color: Colors.grey,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(8, 10, 8,10),
+              child: Text("DEPARTMENTS",
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 0.5,
+                      color: Colors.black)),
+            ),
+            Expanded(
+                child: ListView.builder(
+                  itemCount: depList.length,
+                  itemBuilder: (context,index) => Card(
+                    elevation: 6,
+                    margin: const EdgeInsets.fromLTRB(15, 10, 15,10),
+                    child: ListTile(
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.blue.shade200, width: 0.5),
+                            borderRadius: BorderRadius.circular(5)),
+                        tileColor: Colors.blue.shade400,
+                        textColor: Colors.white,
+                        iconColor: Colors.white,
+                        title: Center(
+                          child: Text(depList[index].toString()),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ContactPage(destination: depList[index].toString())),
+
+                          );
+                        }
+                    ),
+                  ),
+                ),
+              ) ,
+          ],
+        ),
       ),
     );
   }
